e18a99f3bebc317db22f62a09e43978c5161db4f
philips9684 <kiokuphilips9684@gmail.com>
2016-11-05 11:37:53 +0700

    Workshop

---
+++ app/src/main/java/com/example/hp/exworkshop/news_list_Activity.java
@@ -0,0 +1,13 @@
+package com.example.hp.exworkshop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class news_list_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_list);
+    }
+}
